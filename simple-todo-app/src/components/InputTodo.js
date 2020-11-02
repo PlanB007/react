95677cab3e656df27025290df1677e70255439d0
Kevin Rouwkema <kevinrouwkema@gmail.com>
2020-11-02 17:30:04 +0100

    Begin of refactor to react hooks

--- simple-todo-app/src/components/InputTodo.js
+++ simple-todo-app/src/components/InputTodo.js
@@ -1,43 +1,85 @@
-import React, { Component} from "react"
+// import React, { Component} from "react"
+//
+// class InputTodo extends Component {
+//   state = {
+//     title: ""
+//   };
+//
+//   onChange = (e) => {
+//     this.setState({
+//       //this works fine if you have one input field
+//       //title: e.target.value
+//
+//       //this is if you have multiple input fields
+//       [e.target.name]: e.target.value
+//     })
+//   }
+//
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.props.addTodoProps(this.state.title);
+//     // this.setState({
+//       title: ""
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className="form-container">
+//         <input
+//           type="text"
+//           placeholder="add todo"
+//           className="input-text"
+//           value={this.state.title}
+//           name="title" //if you have multiple input fields
+//           onChange={this.onChange}
+//         />
+//         <input type="submit" className="input-submit" value="submit" />
+//       </form>
+//     )
+//   }
+// }
+//
+// export default InputTodo
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
 
-  onChange = (e) => {
-    this.setState({
-      //this works fine if you have one input field
-      //title: e.target.value
+// --------- new version with react hooks ------ //
+import React, {useState} from "react"
 
-      //this is if you have multiple input fields
-      [e.target.name]: e.target.value
+const InputTodo = (props) => {
+
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
     })
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
+    props.addTodoProps(inputText.title)
+    setInputText({
+      title: "",
+    })
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          placeholder="add todo"
-          className="input-text"
-          value={this.state.title}
-          name="title" //if you have multiple input fields
-          onChange={this.onChange}
-        />
-        <input type="submit" className="input-submit" value="submit" />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <input type="submit" className="input-submit" value="Submit" />
+    </form>
+  )
 }
 
 export default InputTodo
